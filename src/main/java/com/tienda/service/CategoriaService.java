312b1a90ce5ce3fe3bf622d8ec917fdc5bcadfab
L-Car09 <luiscubillo189@gmail.com>
2025-06-13 10:51:22 -0600

    semana 5:

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -12,18 +12,30 @@ public class CategoriaService {
     
     //Se crea un objeto de manera única (sólo una instancia) para todo el proyecto y de manera automática
     @Autowired
-    private CategoriaRepository categoriaRepository;
-    
+    private CategoriaRepository categoriaRepository;   
     @Transactional(readOnly=true)
     public List<Categoria> getCategorias(boolean activo) {
-        var lista = categoriaRepository.findAll();
-        
+        var lista = categoriaRepository.findAll();       
         //Se valida si sólo se desean las categorias activas...
         if (activo) {
             //Sólo se quieren activas...
             lista.removeIf(c -> !c.isActivo());
-        }
-        
+        }       
         return lista;
+    }
+    
+    @Transactional(readOnly=true)
+    public Categoria getCategoria(Categoria categoria) {       
+        return categoriaRepository.findById(categoria.getIdCategoria())
+                .orElse(null);
+    }
+    
+    @Transactional
+    public void save(Categoria categoria) {       
+        categoriaRepository.save(categoria);
+    }
+    @Transactional
+    public void delete(Categoria categoria) {       
+        categoriaRepository.delete(categoria);
     }
 }
