8f93d5a50be9c783cd48136727c32801134c05b8
L-Car09 <luiscubillo189@gmail.com>
2025-06-20 10:51:11 -0600

    semana 6

--- src/main/java/com/tienda/controller/ProductoController.java
+++ src/main/java/com/tienda/controller/ProductoController.java
@@ -1,8 +1,8 @@
 package com.tienda.controller;
 
 
-import com.tienda.domain.Categoria;
-import com.tienda.service.CategoriaService;
+import com.tienda.domain.Producto;
+import com.tienda.service.ProductoService;
 import com.tienda.service.FirebaseStorageService;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +17,18 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/categoria")
-public class CategoriaController {
+@RequestMapping("/producto")
+public class ProductoController {
     
     @Autowired
-    private CategoriaService categoriaService; 
+    private ProductoService productoService; 
     
     @GetMapping("/listado")
     public String listado(Model model) {       
-        var lista = categoriaService.getCategorias(false);        
-        model.addAttribute("categorias", lista);
-        model.addAttribute("totalCategorias", lista.size());      
-        return "/categoria/listado";
+        var lista = productoService.getProductos(false);        
+        model.addAttribute("productos", lista);
+        model.addAttribute("totalProductos", lista.size());      
+        return "/producto/listado";
     }
     
     @Autowired
@@ -37,42 +37,42 @@ public class CategoriaController {
     private MessageSource messageSource;
     
     @PostMapping("/guardar")
-    public String guardar(Categoria categoria,
+    public String guardar(Producto producto,
             @RequestParam("imagenFile") MultipartFile imagenFile,
             RedirectAttributes redirectAttributes) {
         if (!imagenFile.isEmpty()){ //Se valida que no esté vacío el archivo de imagen
-            categoriaService.save(categoria);
-            String rutaImagen=firebaseStorageService.cargaImagen(imagenFile,"categoria",categoria.getIdCategoria());
-            categoria.setRutaImagen(rutaImagen);
+            productoService.save(producto);
+            String rutaImagen=firebaseStorageService.cargaImagen(imagenFile,"producto",producto.getIdProducto());
+            producto.setRutaImagen(rutaImagen);
         }
         
             
             
-        categoriaService.save(categoria);
+        productoService.save(producto);
         redirectAttributes.addFlashAttribute("error",messageSource.getMessage("Se realizó la actualización.", null, Locale.getDefault()));    
-        return "redirect:/categoria/listado";
+        return "redirect:/producto/listado";
     }
     @PostMapping("/eliminar")
-    public String eliminar(Categoria categoria, 
+    public String eliminar(Producto producto, 
             RedirectAttributes redirectAttributes) {
-        categoria = categoriaService.getCategoria(categoria);
-        if (categoria==null){
-            redirectAttributes.addFlashAttribute("error",messageSource.getMessage("categoria.error01", null, Locale.getDefault()));
+        producto = productoService.getProducto(producto);
+        if (producto==null){
+            redirectAttributes.addFlashAttribute("error",messageSource.getMessage("producto.error01", null, Locale.getDefault()));
         }else if (false) {
-            redirectAttributes.addFlashAttribute("error",messageSource.getMessage("categoria.error02", null, Locale.getDefault()));
-        }else if (categoriaService.delete(categoria)){
+            redirectAttributes.addFlashAttribute("error",messageSource.getMessage("producto.error02", null, Locale.getDefault()));
+        }else if (productoService.delete(producto)){
              redirectAttributes.addFlashAttribute("error",messageSource.getMessage("mensaje.eliminado", null, Locale.getDefault()));
         } else {
-            redirectAttributes.addFlashAttribute("error",messageSource.getMessage("categoria.error03", null, Locale.getDefault()));
+            redirectAttributes.addFlashAttribute("error",messageSource.getMessage("producto.error03", null, Locale.getDefault()));
         }
-        return "redirect:/categoria/listado";    
+        return "redirect:/producto/listado";    
     }
     
     @PostMapping("/modificar")
-    public String modificar(Categoria categoria, Model model) {       
-        categoria=categoriaService.getCategoria(categoria);
-        model.addAttribute("categoria", categoria);
-        return "/categoria/modifica";
+    public String modificar(Producto producto, Model model) {       
+        producto=productoService.getProducto(producto);
+        model.addAttribute("producto", producto);
+        return "/producto/modifica";
     }
     
     
--- src/main/java/com/tienda/service/ProductoService.java
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -1,22 +1,22 @@
 package com.tienda.service;
 
-import com.tienda.domain.Categoria;
-import com.tienda.repository.CategoriaRepository;
+import com.tienda.domain.Producto;
+import com.tienda.repository.ProductoRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CategoriaService {
+public class ProductoService {
     
     //Se crea un objeto de manera única (sólo una instancia) para todo el proyecto y de manera automática
     @Autowired
-    private CategoriaRepository categoriaRepository;   
+    private ProductoRepository productoRepository;   
     @Transactional(readOnly=true)
-    public List<Categoria> getCategorias(boolean activo) {
-        var lista = categoriaRepository.findAll();       
-        //Se valida si sólo se desean las categorias activas...
+    public List<Producto> getProductos(boolean activo) {
+        var lista = productoRepository.findAll();       
+        //Se valida si sólo se desean las productos activas...
         if (activo) {
             //Sólo se quieren activas...
             lista.removeIf(c -> !c.isActivo());
@@ -25,20 +25,20 @@ public class CategoriaService {
     }
     
     @Transactional(readOnly=true)
-    public Categoria getCategoria(Categoria categoria) {       
-        return categoriaRepository.findById(categoria.getIdCategoria())
+    public Producto getProducto(Producto producto) {       
+        return productoRepository.findById(producto.getIdProducto())
                 .orElse(null);
     }
     
     @Transactional
-    public void save(Categoria categoria) {       
-        categoriaRepository.save(categoria);
+    public void save(Producto producto) {       
+        productoRepository.save(producto);
     }
     @Transactional
-    public boolean delete(Categoria categoria) {  
+    public boolean delete(Producto producto) {  
         try {
-            categoriaRepository.delete(categoria);
-            categoriaRepository.flush();
+            productoRepository.delete(producto);
+            productoRepository.flush();
             return true;
         } catch (Exception e) {
             return false;
